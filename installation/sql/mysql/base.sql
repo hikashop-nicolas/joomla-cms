4dc9f7b53d890e15d071f71bbfba2990aa1bd156
hikari <hikari.software@gmail.com>
2026-06-09 11:52:47 +0200

    Default-enable the customize plugins in the install SQL
    Add the five customize-group plugins (content, language, module, position, view)
    to the installer extensions table in both mysql and postgresql base.sql, enabled,
    so a fresh Joomla install ships them like other default-on plugins. Until now they
    were only inserted manually into the test database.

--- installation/sql/mysql/base.sql
+++ installation/sql/mysql/base.sql
@@ -411,7 +411,12 @@ INSERT INTO `#__extensions` (`package_id`, `name`, `type`, `element`, `folder`, 
 (0, 'plg_workflow_featuring', 'plugin', 'featuring', 'workflow', 0, 1, 1, 0, 1, '', '{}', '', 1, 0),
 (0, 'plg_workflow_notification', 'plugin', 'notification', 'workflow', 0, 1, 1, 0, 1, '', '{}', '', 2, 0),
 (0, 'plg_workflow_publishing', 'plugin', 'publishing', 'workflow', 0, 1, 1, 0, 1, '', '{}', '', 3, 0),
-(0, 'plg_system_guidedtours', 'plugin', 'guidedtours', 'system', 0, 1, 1, 0, 1, '', '{}', '', 15, 0);
+(0, 'plg_system_guidedtours', 'plugin', 'guidedtours', 'system', 0, 1, 1, 0, 1, '', '{}', '', 15, 0),
+(0, 'plg_customize_content', 'plugin', 'content', 'customize', 0, 1, 1, 0, 1, '', '', '', 1, 0),
+(0, 'plg_customize_language', 'plugin', 'language', 'customize', 0, 1, 1, 0, 1, '', '', '', 2, 0),
+(0, 'plg_customize_module', 'plugin', 'module', 'customize', 0, 1, 1, 0, 1, '', '', '', 3, 0),
+(0, 'plg_customize_position', 'plugin', 'position', 'customize', 0, 1, 1, 0, 1, '', '', '', 4, 0),
+(0, 'plg_customize_view', 'plugin', 'view', 'customize', 0, 1, 1, 0, 1, '', '', '', 5, 0);
 
 -- Templates
 INSERT INTO `#__extensions` (`package_id`, `name`, `type`, `element`, `folder`, `client_id`, `enabled`, `access`, `protected`, `locked`, `manifest_cache`, `params`, `custom_data`, `ordering`, `state`) VALUES
